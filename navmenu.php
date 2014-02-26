b7019e74a254fca01eb15a404bfb4b23a7569e43
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-02-26 16:59:45 +0000

    Temporarily removed Age Group selection... Sorry

--- navmenu.php
+++ navmenu.php
@@ -8,13 +8,7 @@
     <body>
         <ul id="navmenu">
             <li><a href=<?php echo getenv('HTTP_HOST');?>><img src="BeXcellent.jpg" width="150px" height="50px" alt="bexcellent"></a></li>
-            <li id="agegroup"><a href=<?php echo getenv('HTTP_HOST') + "ageselect";?>>Age Groups</a>
-                <ul class="sub">
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "5to8"?>>5-8</a></li>
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "8to12"?>>8-12</a></li>
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "12to18"?>>12-18</a></li>
-                </ul>
-            </li>
+
             <li><a href=<?php echo getenv('HTTP_HOST') + "about"?>About BeXcellent</a></li>
             <li><a href=<?php echo getenv('HTTP_HOST') + "news"?>>News</a></li>
             <li><a href=<?php echo getenv('HTTP_HOST') + "gallery"?>>Gallery</a>
