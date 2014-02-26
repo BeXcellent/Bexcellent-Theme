e0357f0d02ef5fc1f39341648d5dc6376d85b20e
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-02-26 16:24:13 +0000

    Fixed URL finding in navbar.php

--- navmenu.php
+++ navmenu.php
@@ -7,25 +7,25 @@
     </head>
     <body>
         <ul id="navmenu">
-            <li><a href=<?php echo $_SERVER['PHP_SELF'] + "articles"?>><img src="BeXcellent.jpg" width="150px" height="50px" alt="bexcellent"></a></li>
-            <li id="agegroup"><a href=<?php echo $_SERVER['PHP_SELF'] + "ageselect"?>>Age Groups</a>
+            <li><a href=<?php echo getenv('HTTP_HOST'); + "articles"?>><img src="BeXcellent.jpg" width="150px" height="50px" alt="bexcellent"></a></li>
+            <li id="agegroup"><a href=<?php echo getenv('HTTP_HOST'); + "ageselect"?>>Age Groups</a>
                 <ul class="sub">
-                    <li><a href=<?php echo $_SERVER['PHP_SELF'] + "5to8"?>>5-8</a></li>
-                    <li><a href=<?php echo $_SERVER['PHP_SELF'] + "8to12"?>>8-12</a></li>
-                    <li><a href=<?php echo $_SERVER['PHP_SELF'] + "12to18"?>>12-18</a></li>
+                    <li><a href=<?php echo getenv('HTTP_HOST'); + "5to8"?>>5-8</a></li>
+                    <li><a href=<?php echo getenv('HTTP_HOST'); + "8to12"?>>8-12</a></li>
+                    <li><a href=<?php echo getenv('HTTP_HOST'); + "12to18"?>>12-18</a></li>
                 </ul>
             </li>
-            <li><a href=<?php echo $_SERVER['PHP_SELF'] + "about"?>About BeXcellent</a></li>
-            <li><a href=<?php echo $_SERVER['PHP_SELF'] + "news"?>>News</a></li>
-            <li><a href=<?php echo $_SERVER['PHP_SELF'] + "gallery"?>>Gallery</a>
+            <li><a href=<?php echo getenv('HTTP_HOST'); + "about"?>About BeXcellent</a></li>
+            <li><a href=<?php echo getenv('HTTP_HOST'); + "news"?>>News</a></li>
+            <li><a href=<?php echo getenv('HTTP_HOST'); + "gallery"?>>Gallery</a>
                 <ul class="sub">
-                    <li><a href=<?php echo $_SERVER['PHP_SELF'] + "photos"?>>Photos</a></li>
-                    <li><a href=<?php echo $_SERVER['PHP_SELF'] + "videos"?>>Videos</a></li>
+                    <li><a href=<?php echo getenv('HTTP_HOST'); + "photos"?>>Photos</a></li>
+                    <li><a href=<?php echo getenv('HTTP_HOST'); + "videos"?>>Videos</a></li>
                 </ul>
             </li>
-            <li><a href=<?php echo $_SERVER['PHP_SELF'] + "articles"?>>Articles</a></li>
-            <li><a href=<?php echo $_SERVER['PHP_SELF'] + "games"?>>Games</a></li>
-            <li><a href=<?php echo $_SERVER['PHP_SELF'] + "parents"?>>Parents</a></li>
+            <li><a href=<?php echo getenv('HTTP_HOST'); + "articles"?>>Articles</a></li>
+            <li><a href=<?php echo getenv('HTTP_HOST'); + "games"?>>Games</a></li>
+            <li><a href=<?php echo getenv('HTTP_HOST'); + "parents"?>>Parents</a></li>
         </ul>
     </body>
 </html>
