5c4ec60c3d00addfc0a1d4df617954d377e70acb
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-02-27 16:24:22 +0000

    Finally got navmenu urls working!

--- navmenu.php
+++ navmenu.php
@@ -5,25 +5,25 @@
     </head>
     <body>
         <ul id="navmenu" display="inline">
-            <li><a href=<?php echo site_url();?>><img src="BeXcellent.jpg" width="150px" height="50px" alt="BeXcellent"></a></li>
-            <li id="agegroup" display="inline"><a href="<?php echo site_url();?>/ageselect">Age Groups</a>
+            <li><a href="/"><img src="BeXcellent.jpg" width="150px" height="50px" alt="BeXcellent"></a></li>
+            <li id="agegroup" display="inline"><a href="/ageselect">Age Groups</a>
                 <ul class="sub">
-                    <li><a href="<?php echo site_url();?>/5to8">5-8</a></li>
-                    <li><a href="<?php echo site_url();?>/8to12">8-12</a></li>
-                    <li><a href="<?php echo site_url();?>/12to18"?>>12-18</a></li>
+                    <li><a href="/5to8">5-8</a></li>
+                    <li><a href="/8to12">8-12</a></li>
+                    <li><a href="/12to18"?>12-18</a></li>
                 </ul>
             </li>
-            <li><a href="<?php echo site_url();?>/news">News</a></li>
-            <li><a href="<?php echo site_url();?>/gallery">Gallery</a>
+            <li><a href="/news">News</a></li>
+            <li><a href="/gallery">Gallery</a>
                 <ul class="sub">
-                    <li><a href="<?php echo site_url();?>/photos">Photos</a></li>
-                    <li><a href="<?php echo site_url();?>/videos">Videos</a></li>
+                    <li><a href="/photos">Photos</a></li>
+                    <li><a href="/videos">Videos</a></li>
                 </ul>
             </li>
-            <li><a href="<?php echo site_url();?>/articles">Articles</a></li>
-            <li><a href="<?php echo site_url();?>/games">Games</a></li>
-            <li><a href="<?php echo site_url();?>/parents">Parents</a></li>
- <li><a href="<?php echo site_url();?>/about"?>About</a></li>
+            <li><a href="/articles">Articles</a></li>
+            <li><a href="/games">Games</a></li>
+            <li><a href="/parents">Parents</a></li>
+ <li><a href="/about"?>About</a></li>
         </ul>
     </body>
 </html>
