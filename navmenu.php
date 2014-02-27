59f7f5fd56ccf4de90d102ed0de19d77349b92a5
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-02-27 15:37:14 +0000

    Changed the way URL getting works in the navmenu.php - Maybe it will work now

--- navmenu.php
+++ navmenu.php
@@ -5,25 +5,25 @@
     </head>
     <body>
         <ul id="navmenu" display="inline">
-            <li><a href=<?php echo getenv('HTTP_HOST');?>><img src="BeXcellent.jpg" width="150px" height="50px" alt="BeXcellent"></a></li>
-            <li id="agegroup" display="inline"><a href=<?php echo getenv('HTTP_HOST') + "ageselect";?>>Age Groups</a>
+            <li><a href=<?php echo site_url();?>><img src="BeXcellent.jpg" width="150px" height="50px" alt="BeXcellent"></a></li>
+            <li id="agegroup" display="inline"><a href=<?php echo site_url() + "ageselect";?>>Age Groups</a>
                 <ul class="sub">
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "5to8"?>>5-8</a></li>
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "8to12"?>>8-12</a></li>
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "12to18"?>>12-18</a></li>
+                    <li><a href=<?php echo site_url() + "/5to8"?>>5-8</a></li>
+                    <li><a href=<?php echo site_url() + "/8to12"?>>8-12</a></li>
+                    <li><a href=<?php echo site_url() + "/12to18"?>>12-18</a></li>
                 </ul>
             </li>
-            <li><a href=<?php echo getenv('HTTP_HOST') + "news"?>>News</a></li>
-            <li><a href=<?php echo getenv('HTTP_HOST') + "gallery"?>>Gallery</a>
+            <li><a href=<?php echo site_url() + "/news"?>>News</a></li>
+            <li><a href=<?php echo site_url() + "/gallery"?>>Gallery</a>
                 <ul class="sub">
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "photos"?>>Photos</a></li>
-                    <li><a href=<?php echo getenv('HTTP_HOST') + "videos"?>>Videos</a></li>
+                    <li><a href=<?php echo site_url() + "/photos"?>>Photos</a></li>
+                    <li><a href=<?php echo site_url() + "/videos"?>>Videos</a></li>
                 </ul>
             </li>
-            <li><a href=<?php echo getenv('HTTP_HOST') + "articles"?>>Articles</a></li>
-            <li><a href=<?php echo getenv('HTTP_HOST') + "games"?>>Games</a></li>
-            <li><a href=<?php echo getenv('HTTP_HOST') + "parents"?>>Parents</a></li>
- <li><a href=<?php echo getenv('HTTP_HOST') + "about"?>About</a></li>
+            <li><a href=<?php echo site_url() + "/articles"?>>Articles</a></li>
+            <li><a href=<?php echo site_url() + "/games"?>>Games</a></li>
+            <li><a href=<?php echo site_url() + "/parents"?>>Parents</a></li>
+ <li><a href=<?php echo site_url() + "/about"?>About</a></li>
         </ul>
     </body>
 </html>
