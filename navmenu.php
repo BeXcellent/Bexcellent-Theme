9a970e56d6ec11d8ece1edeb0003449d4c98a21f
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-01 14:09:23 +0000

    Menu.css should only affect navmenu

--- navmenu.php
+++ navmenu.php
@@ -4,6 +4,7 @@
         <link rel="stylesheet" type="text/css" target="_parent" href="css/menu.css">
     </head>
     <body>
+	<div id="navdiv">
         <ul id="navmenu" display="inline">
             <li><a target="_parent" href="/"><img src="BeXcellent.jpg" width="150px" height="50px" alt="BeXcellent"></a></li>
             <li display="inline"><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/ageselect">Age Selection</a>
@@ -16,5 +17,6 @@
             <li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents">Parents</a></li>
  <li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about"?>About</a></li>
         </ul>
+	</div
     </body>
 </html>
