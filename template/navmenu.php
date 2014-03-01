b1d6ed3ba4c6578356373d46913aeda50f9eed52
pm990320 <patmenlove@gmail.com>
2014-03-01 21:41:48 +0000

    a few more adjustments

--- template/navmenu.php
+++ template/navmenu.php
@@ -2,15 +2,13 @@
 			<li id="superli"> <p>Menu</p>
 				<ul class="sub">
 					<li><a target="_parent" href="/">Home</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/ageselect">Age</a>
-					</li>
+					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about">About</a></li>
+					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/age-selection">Age</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news">News</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery">Gallery</a>
-					</li>
+					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/photos">Gallery</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles">Articles</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games">Games</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents">Parents</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about">About</a></li>
 				</ul>
 			</li>
 		</ul>
