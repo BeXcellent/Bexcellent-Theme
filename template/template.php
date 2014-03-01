b07f1ad02078f3a2bad10b63777383e68b564fa1
pm990320 <patmenlove@gmail.com>
2014-03-01 21:08:05 +0000

    making more changes

--- template/template.php
+++ template/template.php
@@ -14,4 +14,22 @@ get_header(); ?>
 		</div>
 		<div id="title"> <img src="http://95.85.42.215/wp-content/uploads/2014/03/title.jpg" alt="BeXcellent"> </div>
 		<div id="sidebar"> where the sidebar will be </div>
-		<div id="content">
+		<div id="contentmine"
+
+			<div id="primary" class="site-content">
+				<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if ( has_post_thumbnail() ) : ?>
+					<div class="entry-page-image">
+						<?php the_post_thumbnail(); ?>
+					</div><!-- .entry-page-image -->
+				<?php endif; ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+				</div><!-- #content -->
+			</div><!-- #primary -->
+		</div>
