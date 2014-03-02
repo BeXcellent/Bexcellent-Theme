9cf7576aecef790201deca5bc686c33a19836ccd
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-02 21:34:41 +0000

    Moved files in /template to the right places

--- navmenu.php
+++ navmenu.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <link rel="stylesheet" type="text/css" type="text/css" href="css/menu.css">
-    </head>
-    <body>
 		<ul id="super">
-			<li id="superli">
+			<li id="superli"> <p>Menu</p>
 				<ul class="sub">
 					<li><a target="_parent" href="/">Home</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/ageselect">Age</a>
-					</li>
+					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about">About</a></li>
+					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/age-selection">Age</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news">News</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery">Gallery</a>
-					</li>
+					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery">Gallery</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles">Articles</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games">Games</a></li>
 					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents">Parents</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about">About</a></li>
 				</ul>
 			</li>
 		</ul>
-	</body>
-</html>
--- page-templates/template.php
+++ page-templates/template.php
@@ -2,7 +2,7 @@
 /**
  * Template Name: Template
  *
- * Description: blabla
+ * Description: New navbar testing stuff
  *
  * @package WordPress
  * @subpackage BeXcellent
@@ -10,8 +10,26 @@
  */
 get_header(); ?>
 		<div id="menu">
-			<?php include 'navmenu.php'; ?>
+			<?php include './../navmenu.php'; ?>
 		</div>
-		<div id="title"> <img src="wp-content/uploads/2014/03/title.jpg" alt="BeXcellent"> </div>
-		<div id="sidebar"> where the sidebar will be </div>
-		<div id="content"> where the content will be
+		<div id="title"> <img src="./../title.jpg" alt="BeXcellent"> </div>
+		<div id="sidebar"> <?php get_sidebar(); ?> </div>
+		<div id="contentmine"
+
+			<div id="primary" class="site-content">
+				<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php if ( has_post_thumbnail() ) : ?>
+					<div class="entry-page-image">
+						<?php the_post_thumbnail(); ?>
+					</div><!-- .entry-page-image -->
+				<?php endif; ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+				</div><!-- #content -->
+			</div><!-- #primary -->
+		</div>
