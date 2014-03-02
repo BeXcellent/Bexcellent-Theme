3cdc91b66a2ecb8fa6e76a0e9f995f2720bdfb3c
pm990320 <patmenlove@gmail.com>
2014-03-02 22:12:15 +0000

    ..

--- navmenu.php
+++ navmenu.php
@@ -1,14 +1,14 @@
-		<ul id="super">
-			<li id="superli"> <p>Menu</p>
-				<ul class="sub">
-					<li><a target="_parent" href="/">Home</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about">About</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/age-selection">Age</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news">News</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery">Gallery</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles">Articles</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games">Games</a></li>
-					<li><a target="_parent" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents">Parents</a></li>
-				</ul>
-			</li>
+<ul id="super">
+	<li id="superli"> <p>Menu</p>
+		<ul class="sub">
+			<li><a href="/">Home</a></li>
+			<li><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/about">About</a></li>
+			<li><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/age-selection">Age</a></li>
+			<li><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/news">News</a></li>
+			<li><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/gallery">Gallery</a></li>
+			<li><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/articles">Articles</a></li>
+			<li><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/games">Games</a></li>
+			<li><a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URL']; ?>/parents">Parents</a></li>
 		</ul>
+	</li>
+</ul>
