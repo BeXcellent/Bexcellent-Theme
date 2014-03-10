c887da974f16472ad40449fe94ffe42102981d69
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-10 19:17:05 +0000

    Resized sadface in 404

--- 404.php
+++ 404.php
@@ -14,12 +14,14 @@ get_header();
 			<section class="error-404 not-found">
 
 				<div class="page-content">
-				<img src="/wp-content/themes/Bexcellent-Theme/sadface.jpg" align="center" />
+				<img src="/wp-content/themes/Bexcellent-Theme/sadface.jpg" align="center" height=250 weight=250 />
 					<h1 color="#C00000" align="center">Well this is embarrasing...</h1>
 					<h2 color="#484848" align="center">We couldn't find that page!</h2>
 					<h3 color="#484848" align="center">Try searching for it.</h3>
+				<div align="center">
 					<?php get_search_form(); ?>
-					<h3 color="#484848" align="center">Or (If you want to) You can look at this picture about CfE instead...</h3>
+			</div>					
+<h3 color="#484848" align="center">Or (If you want to) You can look at this picture about CfE instead...</h3>
 					<img src="/wp-content/themes/Bexcellent-Theme/cfe.jpg" align="center" />
 				</div>
 			</section>
