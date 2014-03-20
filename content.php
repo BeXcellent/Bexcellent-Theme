219049412519f06a521af0d2dc1a906a2042c0a7
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-20 17:48:11 +0000

    Removed category and tag text

--- content.php
+++ content.php
@@ -33,51 +33,7 @@
 
 	<footer class="entry-meta">
 		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = wp_get_post_categories(get_post_id());
-				if ( $categories_list && bexcellent_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-<<<<<<< HEAD
-				<li class="dropdown">Categories
-        				<ul>
-            					<?php
-							foreach ( $tags_list as $tag ) {
-								echo '<li class="dropdown">' . $tag . '</li>';
-							}
-							unset($tag);
-						?>
-       					</ul>
-    				</li>
-=======
-				<?php printf( __( 'Posted in %1$s', 'bexcellent' ), $categories_list ); ?>
->>>>>>> parent of bf7c4b5... Category and tag lists (should) now be drop-downs
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'bexcellent' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-<<<<<<< HEAD
-				<li class="dropdown">Tags
-        				<ul>
-            					<?php
-						foreach ( $tags_list as $tag ) {
-							echo '<li class="dropdown">' . $tag . '</li>';
-						}
-						unset($tag);
-						?>
-       					</ul>
-    				</li>
-=======
-				<?php printf( __( 'Tagged %1$s', 'bexcellent' ), $tags_list ); ?>
->>>>>>> parent of bf7c4b5... Category and tag lists (should) now be drop-downs
-			</span>
-			<?php endif; // End if $tags_list ?>
+			<!-- Stuff -->
 		<?php endif; // End if 'post' == get_post_type() ?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
