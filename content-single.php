31f042ec12a8b6afc16720fe0aac3ba5150ea926
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-20 17:19:44 +0000

    DHAJSH

--- content-single.php
+++ content-single.php
@@ -24,39 +24,43 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'bexcellent' ) );
+		<h1>Hi</h1>
 
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'bexcellent' ) );
-
-			if ( ! bexcellent_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'bexcellent' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'bexcellent' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'bexcellent' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'bexcellent' );
-				}
-
-			} // end check for categories on this blog
-
-			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink()
-			);
-		?>
-
-		<?php edit_post_link( __( 'Edit', 'bexcellent' ), '<span class="edit-link">', '</span>' ); ?>
+					<?php
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ', ', 'bexcellent' ) );
+				if ( $categories_list && bexcellent_categorized_blog() ) :
+			?>
+			<span class="cat-links">
+				<li class="dropdown">Categories
+        				<ul class="dropdown">
+            					<?php
+							foreach ( $tags_list as $tag ) {
+								echo '<li class="dropdown">' . $tag . '</li>';
+							}
+							unset($tag);
+						?>
+       					</ul>
+    				</li>
+			</span>
+			<?php endif; // End if categories ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'bexcellent' ) );
+				if ( $tags_list ) :
+			?>
+			<span class="tags-links">
+				<li class="dropdown">Tags
+        				<ul class="dropdown">
+            					<?php
+						foreach ( $tags_list as $tag ) {
+							echo '<li class="dropdown">' . $tag . '</li>';
+						}
+						unset($tag);
+						?>
+       					</ul>
+    				</li>
+			</span>
+			<?php endif; // End if $tags_list ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
