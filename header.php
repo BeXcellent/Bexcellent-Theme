29d1c1f567bfd9260a53f35b861f7c52f4eb111d
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-22 11:43:55 +0000

    ...

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
-
+<?php echo do_shortcode('[google-translator]'); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
