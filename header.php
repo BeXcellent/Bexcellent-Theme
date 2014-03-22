dc2fd1918566c1e089bd1ea1128b761dda07abf4
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-22 14:16:58 +0000

    Randomized background stuff

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
+
+
 <?php
 /**
  * The Header for our theme.
@@ -6,7 +8,12 @@
  *
  * @package BeXcellent
  */
-?><!DOCTYPE html>
+
+  $bg = array('bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png', 'bg8.png', 'bg9.png', 'bg10.png', 'bg11.png' );
+  $i = rand(0, count($bg)-1);
+  $selectedBg = "$bg[$i]";
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -16,6 +23,11 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="stylesheet" type="text/css" href="/wp-content/themes/Bexcellent-Theme/style.css">
 <script src="/wp-content/themes/Bexcellent-Theme/js/bubble.js"></script>
+<style type="text/css">
+body{
+background: url('./wp-content/themes/BeXcellent-Theme/backgrounds/<?php echo $selectedBg; ?>') no-repeat;
+}
+</style>
 <?php wp_head(); ?>
 </head>
 
