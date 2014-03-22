4b3a4b48aec3adb31b309a66e4af0a70e45e0693
Oscar Shrimpton <shrimptonoscar@gmail.com>
2014-03-22 14:49:12 +0000

    Removed random backgrounds

--- header.php
+++ header.php
@@ -8,10 +8,6 @@
  *
  * @package BeXcellent
  */
-
-  $bg = array('bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png', 'bg8.png', 'bg9.png', 'bg10.png', 'bg11.png' );
-  $i = rand(0, count($bg)-1);
-  $selectedBg = "$bg[$i]";
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -23,13 +19,6 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="stylesheet" type="text/css" href="/wp-content/themes/Bexcellent-Theme/style.css">
 <script src="/wp-content/themes/Bexcellent-Theme/js/bubble.js"></script>
-<style type="text/css">
-body{
-background-image: url("./wp-content/themes/BeXcellent-Theme/backgrounds/<?php echo $selectedBg; ?>");
-background-repeat: no-repeat;
-background-size: cover;
-}
-</style>
 <?php wp_head(); ?>
 </head>
 
