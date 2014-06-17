5c81ee9697ad4b1760a27152a48ea7530f6f158c
Oscar Shrimpton <oscar.shrimpton.personal@gmail.com>
2014-06-17 21:07:06 +0100

    ...

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
 	<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar' ); ?>
+		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</div>
 <?php endif; ?>
 
