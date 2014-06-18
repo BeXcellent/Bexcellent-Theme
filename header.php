075d37e725ac0c01f2726cf2e334b72e7bd6d79a
Oscar Shrimpton <oscar.shrimpton.personal@gmail.com>
2014-06-18 18:33:31 +0100

    ...

--- header.php
+++ header.php
@@ -11,9 +11,10 @@
 <html <?php language_attributes(); ?> >
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
